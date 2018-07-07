2aa73ad1b3e373e89c4086e5ef47a39389cd9b29
karthikrock <karthik@Karthiks-MacBook-Pro.local>
2018-07-07 17:48:35 -0500

    testing docker ci

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /public csk
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
